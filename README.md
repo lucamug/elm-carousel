39620d5412a06680894da1ff33a5fc19f8d32ab0
Luca <lucamug@gmail.com>
2017-06-03 20:31:41 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# ![Elm Carousel](.png)
+
+### [Demo](https://lucamug.github.io/elm-carousel)
+
+# How it works
+
+Check out [the full writeup](https://medium...!
+
+# Getting started
+
+If you don't already have `elm` and `elm-live`:
+
+> npm install -g elm elm-live
+
+Then, to build everything:
+
+> elm-live --output=elm.js src/Main.elm --pushstate --open --debug
+
+(Leave off the `--debug` if you don't want the time-traveling debugger.)
---
+++ src/Main.elm
@@ -0,0 +1,141 @@
+port module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Navigation
+import Json.Encode exposing (Value)
+
+
+port urlChange : String -> Cmd msg
+
+
+initialModel =
+    { history = []
+    }
+
+
+type alias Model =
+    { history : List Navigation.Location
+    }
+
+
+type Msg
+    = UrlChange Navigation.Location
+
+
+init : Navigation.Location -> ( Model, Cmd Msg )
+init location =
+    ( initialModel
+    , Cmd.none
+      -- , InitialCmd.initialCmd
+    )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        UrlChange location ->
+            ( { model | history = location :: model.history }
+            , Cmd.batch
+                [ (urlChange location.hash)
+                ]
+            )
+
+
+view : Model -> Html Msg
+view model =
+    let
+        currenthash =
+            currentHash (List.head model.history)
+    in
+        div [ style [ ( "background-color", "#444" ) ] ]
+            [ div
+                [ style
+                    [ ( "position", "fixed" )
+                    , ( "background-color", "#444" )
+                    , ( "color", "white" )
+                    , ( "opacity", "0.5" )
+                    , ( "top", "0" )
+                    ]
+                ]
+                [ div
+                    []
+                    [ ul []
+                        (List.map viewLink
+                            [ "top"
+                            , "carousel"
+                            , "post"
+                            , "comment"
+                            ]
+                        )
+                    ]
+                ]
+            , if currenthash == "#carousel" then
+                div []
+                    [ h1 [] [ text "Carousel" ]
+                    , div [ class "carousel" ]
+                        [ div [ class "carousel-cell" ]
+                            []
+                        , div [ class "carousel-cell" ]
+                            []
+                        , div [ class "carousel-cell" ]
+                            []
+                        , div [ class "carousel-cell" ]
+                            []
+                        , div [ class "carousel-cell" ]
+                            []
+                        ]
+                    ]
+              else if currenthash == "#top" then
+                h1 [] [ text "Top" ]
+              else if currenthash == "#post" then
+                h1 [] [ text "Post" ]
+              else if currenthash == "#comment" then
+                h1 [] [ text "Comment" ]
+              else
+                div [] []
+            ]
+
+
+currentHash currentPage =
+    case currentPage of
+        Just location ->
+            location.hash
+
+        Nothing ->
+            ""
+
+
+viewLink : String -> Html msg
+viewLink name =
+    li
+        [ style
+            [ ( "display", "inline-block" )
+            , ( "padding", "0 10px" )
+            ]
+        ]
+        [ a
+            [ href ("#" ++ name)
+            , style [ ( "color", "inherit" ) ]
+            ]
+            [ text name ]
+        ]
+
+
+viewLocation : Navigation.Location -> Html msg
+viewLocation location =
+    li [] [ text (location.pathname ++ location.hash) ]
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+main =
+    Navigation.program UrlChange
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
